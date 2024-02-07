2fdd0569b7d2c7cd3203a9a3bfcd73d6abef7ded
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2024-02-07 16:42:05 +0700

    feat: define color, typografi

--- app/src/main/java/com/example/shopapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/shopapp/ui/theme/Type.kt
@@ -2,33 +2,49 @@ package com.example.shopapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.shopapp.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular)
+)
+
+val Raleway = FontFamily(
+    Font(R.font.raleway_bold, FontWeight.Bold),
+    Font(R.font.raleway_semibold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+    headlineLarge = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 26.sp,
+    ),
+    headlineSmall =    TextStyle(
+        fontFamily = Raleway,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     )
-    */
+
 )
