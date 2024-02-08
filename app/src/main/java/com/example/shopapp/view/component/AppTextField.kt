9805185807be969ed68400c2a7c85ec55db48d11
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2024-02-08 19:18:24 +0700

    feat: finish create default app textfield

---
+++ app/src/main/java/com/example/shopapp/view/component/AppTextField.kt
@@ -0,0 +1,118 @@
+package com.example.shopapp.view.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shopapp.ui.theme.Grey
+import com.example.shopapp.ui.theme.LightGrey
+import com.example.shopapp.ui.theme.ShopAppTheme
+import com.example.shopapp.view.screen.LoginScreen
+
+@Composable
+fun DefaultAppTextField(
+    modifier: Modifier = Modifier,
+    label: String = "",
+    value: String,
+    isSecureText: Boolean = false,
+    placeHolder: String = "",
+    onValueChange: (String) -> Unit,
+    isError: Boolean = false,
+    trailingIcon: @Composable() (() -> Unit)? = null,
+    keyboardActions: KeyboardActions = KeyboardActions { },
+    keyboardOptions: KeyboardOptions = KeyboardOptions()
+
+) {
+    Column {
+        Text(
+            text = label, style = MaterialTheme.typography.headlineSmall.copy(
+                fontSize = 16.sp, fontWeight = FontWeight.Medium
+            ), modifier = Modifier.padding(start = 5.dp, bottom = 12.dp)
+        )
+        TextField(
+            modifier = modifier,
+            value = value,
+            isError = isError,
+            textStyle = MaterialTheme.typography.bodyMedium.copy(
+                fontSize = 14.sp, color = Color.Black
+            ),
+            onValueChange = onValueChange,
+            placeholder = {
+                Text(
+                    text = placeHolder, style = MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 14.sp, color = Grey
+                    )
+                )
+            },
+
+            shape = RoundedCornerShape(14.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = LightGrey.copy(
+                    alpha = 0.1f
+                ),
+                unfocusedContainerColor = LightGrey.copy(
+                    alpha = 0.1f
+                ),
+                focusedTextColor = Color.Transparent,
+                unfocusedTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            visualTransformation = if (isSecureText) PasswordVisualTransformation() else VisualTransformation.None,
+            trailingIcon = trailingIcon,
+            keyboardActions = keyboardActions,
+            keyboardOptions = keyboardOptions
+
+        )
+        AnimatedVisibility(
+            visible = isError, enter = fadeIn(animationSpec = tween(2000))
+        ) {
+            Text(
+                text = "please fill!!",
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    fontSize = 12.sp, color = Color.Red.copy(
+                        alpha = 0.6f
+                    ), fontWeight = FontWeight.Medium
+                ),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShopAppTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+        ) {
+            DefaultAppTextField(value = "", onValueChange = {})
+        }
+    }
+}
--- app/src/main/java/com/example/shopapp/view/screen/Login.kt
+++ app/src/main/java/com/example/shopapp/view/screen/Login.kt
@@ -3,32 +3,70 @@ package com.example.shopapp.view.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shopapp.Greeting
+import com.example.shopapp.R
+import com.example.shopapp.ui.theme.LightGrey
 import com.example.shopapp.ui.theme.ShopAppTheme
+import com.example.shopapp.view.component.DefaultAppTextField
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
     Scaffold(
         modifier = modifier.fillMaxSize()
     ) {
-        Column(modifier = Modifier
-            .padding(it)
-            .fillMaxSize().padding(top = 70.dp),
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .padding(top = 70.dp),
 
             horizontalAlignment = Alignment.CenterHorizontally
 
-            ) {
-            Text(text = "Halo bang")
+        ) {
+            Text(
+                text = stringResource(id = R.string.title_sign_in),
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Divider(modifier = Modifier.height(10.dp), color = Color.Transparent)
+            Text(
+                text = stringResource(id = R.string.subtitle_auth),
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    color = LightGrey
+                )
+            )
+            Divider(color= Color.Transparent, modifier = Modifier.height(50.dp))
+            DefaultAppTextField(
+                modifier = Modifier.fillMaxWidth(),
+                label= "Email Address",
+                isError = false,
+                value = "",
+                placeHolder = stringResource(id = R.string.placeholder_email),
+                onValueChange = {}
+            )
+            DefaultAppTextField(
+                modifier = Modifier.fillMaxWidth(),
+                label= "Email Address",
+                isError = false,
+                value = "",
+                placeHolder = stringResource(id = R.string.placeholder_email),
+                onValueChange = {}
+            )
 
         }
     }
